b694db5107e267677956f70720641943074e3928
Vaith <vaithwee@yeah.net>
2017-03-14 15:13:24 +0800

    add heroku profile

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function(req, res){
     res.render('index');
 });

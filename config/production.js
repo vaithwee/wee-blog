8943941f9f4398428b4c3826272f58ae40dca687
Vaith <vaithwee@yeah.net>
2020-02-25 08:30:01 +0800

    Remove useless code

--- config/production.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    port: process.env.PORT || 8090,
-    mongo: 'mongodb://vaithwee:3a24ca224c@ds131320.mlab.com:31320/wee-blog',
-    session: {
-        secret: 'wee-blog',
-        key: '12fa213edf2',
-        maxAge: 2592000000
-    }
-}

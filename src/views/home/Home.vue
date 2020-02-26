5b4b64f91254baea7bef213af825273f02963ab7
Vaith <vaithwee@yeah.net>
2020-02-26 23:46:29 +0800

    Add future display

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -6,7 +6,55 @@
                 <h1>I'm <span class="bold">Wee</span>, I love <span class="bold">Coding</span>.</h1>
             </div>
         </section>
-        <div style="background-color: red;height: 150px;"></div>
+        <div style="background-color: white;padding: 100px 10px;margin-left: auto;margin-right: auto">
+            <el-row>
+                <el-col :xs="{span:24}" :sm="{span:8}" :md="{span:6, offset:3}">
+                    <div class="list-item">
+                        <div class="icon">
+                            <i class="el-icon-star-off"></i>
+                        </div>
+                        <div class="desc">
+                            <h2>Based on Bootstrap 4</h2>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                tempor incididunt ut labore.
+                            </p>
+                            <a href="#" class="more">Learn More</a>
+                        </div>
+                    </div>
+                </el-col>
+                <el-col :xs="{span:24}" :sm="{span:8}" :md="{span:6}">
+                    <div class="list-item">
+                        <div class="icon">
+                            <i class="el-icon-star-off"></i>
+                        </div>
+                        <div class="desc">
+                            <h2>Based on Bootstrap 4</h2>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                tempor incididunt ut labore.
+                            </p>
+                            <a href="#" class="more">Learn More</a>
+                        </div>
+                    </div>
+                </el-col>
+                <el-col :xs="{span:24}" :sm="{span:8}" :md="{span:6}">
+                    <div class="list-item">
+                        <div class="icon">
+                            <i class="el-icon-star-off"></i>
+                        </div>
+                        <div class="desc">
+                            <h2>Based on Bootstrap 4</h2>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                tempor incididunt ut labore.
+                            </p>
+                            <a href="#" class="more">Learn More</a>
+                        </div>
+                    </div>
+                </el-col>
+            </el-row>
+        </div>
         <div style="background-color: orange;height: 150px;"></div>
         <div style="background-color: gray;height: 150px;"></div>
         <div style="background-color: brown;height: 150px;"></div>
@@ -103,4 +151,46 @@
     .text h1 {
         margin-top: 25vh;
     }
+
+    .list-item {
+        display: flex;
+        display: -webkit-flex;
+        width: 100%;
+        text-align: left;
+    }
+
+    .icon {
+        flex: 0 0 65px;
+        -webkit-flex: 0 0 65px;
+        font-size: 45px;
+        line-height: 0;
+        color: #ccc;
+    }
+
+    .desc {
+        flex: 1;
+        -webkit-flex: 1;
+    }
+
+    .list-item .desc h2 {
+        font-size: 16px;
+        text-transform: uppercase;
+        font-weight: 800;
+        letter-spacing: .5px;
+        margin-bottom: 10px;
+        font-family: inherit;
+        line-height: 1.1;
+    }
+
+    .list-item .desc p {
+        line-height: 28px;
+        font-size: 16px;
+        margin-bottom: 0;
+    }
+
+    .list-item .desc .more {
+        display: inline-block;
+        margin-top: 10px;
+    }
+
 </style>

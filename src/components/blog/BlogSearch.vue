597800d253c25d93b8ac0f15ad679bf2e0b22a07
Vaith <vaithwee@yeah.net>
2020-08-24 17:46:01 +0800

    Adjust search input ui and css to common

--- src/components/blog/BlogSearch.vue
+++ src/components/blog/BlogSearch.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="bs-content">
-    <label>
-      <input type="text" class="bs-input" placeholder="请输入关键词" ref="input" @keypress.enter="searchBlog"/>
-    </label>
+  <div class="bs-content shadow-border">
+    <div class="bs-input-content">
+      <label>
+        <input type="text" class="bs-input" placeholder="请输入关键词" ref="input" @keypress.enter="searchBlog" />
+      </label>
+    </div>
     <button type="submit" class="bs-button" @click="searchBlog">搜索</button>
   </div>
 </template>
--- src/views/search/Search.vue
+++ src/views/search/Search.vue
@@ -3,13 +3,18 @@
     <route-tip-view/>
     <div class="content">
       <div class="bs-content">
-        <label>
-          <input v-model="keyword" type="text" class="bs-input" placeholder="请输入关键词" ref="input"/>
-        </label>
-        <button type="submit" class="bs-button">搜索</button>
+        <div class="shadow-border sr-content">
+          <div style="flex: 1">
+            <label>
+              <input v-model="keyword" type="text" class="bs-input" placeholder="请输入关键词" ref="input" @keypress.enter="search"/>
+            </label>
+          </div>
+          <button type="submit" class="bs-button" @click="search">搜索</button>
+        </div>
+
       </div>
       <div v-if="isNone" class="sr-none">
-          <h1>没有搜索结果</h1>
+        <h1>没有搜索结果</h1>
       </div>
       <home-article-list :list="list"/>
     </div>
@@ -23,7 +28,7 @@
   import HomeArticleListItem from "../../components/home/HomeArticleListItem";
 
   export default {
-    name: "serach",
+    name: "search",
     components: {
       HomeArticleListItem,
       RouteTipView,
@@ -42,6 +47,14 @@
     methods: {
       searchArticleByKeyword() {
         let keyword = this.$route.query.keyword;
+        this.keyword = keyword;
+        ArticleAPI.searchBlog(keyword, 0, 20).then(res => {
+          this.list = res.data.data;
+          this.isNone = this.list.length === 0;
+        })
+      },
+      search() {
+        let keyword = this.$refs.input.value;
         this.keyword = keyword;
         ArticleAPI.searchBlog(keyword, 0, 20).then(res => {
           this.list = res.data.data;
@@ -66,20 +79,36 @@
   }
 
   .bs-input {
-    width: 300px;
-    height: 30px;
+    width: 100%;
+    height: 55px;
     font-weight: bold;
+    min-height: 10px;
+    min-width: 10px;
     font-size: 20px;
+    padding: 0 10px;
+    border: none;
+    flex: 1;
   }
 
   .bs-button {
-    width: 52px;
-    height: 52px;
+    width: 70px;
+    height: 55px;
+    margin-top: 0;
+    border: none;
+    padding: 0;
   }
 
   .sr-none {
     width: 100%;
     height: 50vh;
     margin: auto;
+  }
+
+  .sr-content {
+    display: flex;
+    border: 1px solid #f0f0f0;
+    margin: auto;
+    flex: 1;
+    /*width: 50vw;*/
   }
 </style>
